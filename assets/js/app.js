dc6c2eac194b0bade929e723d22e101ad7788bf1
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-08-10 15:24:00 +0530

    change css

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,24 @@ let btn = document.querySelector(".circle")
   console.log("button");
   console.log(".circle");
 
-let val = 1;
+let val = 2;
 mybtn.addEventListener("click", function () {
-    if (val === 1) {
+    if (val === 2) {
         btn.classList.add("fun");
         btn.classList.add("circle2");
+        mybtn.classList.add("button2")
         mybtn.classList.remove("font")
         mybtn.classList.remove("button")
         mybtn.innerHTML = "off";
-        val = 0
+        val = 1
     }
-    else if (val === 0) {
-        btn.classList.remove("fun");
-        btn.classList.remove("circle2");
+    else if (val === 1) {
         mybtn.classList.add("font")
         mybtn.classList.add("button")
+        btn.classList.remove("fun");
+        btn.classList.remove("circle2");
+        mybtn.classList.remove("button2")
         mybtn.innerHTML = "on";
-        val = 1
+        val = 2
     }
 })
